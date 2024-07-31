49fecb1ebef7b90e42a0b23fefdadd03e519f817
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-07-31 19:00:42 +0530

    Create k-pattern.java

---
+++ k-pattern.java
@@ -0,0 +1,63 @@
+k-pattern
+
+
+
+package javaapplication8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author meila
+ */
+public class JavaApplication8 {
+
+    public static void call(int a){
+        int i,j;
+        for(i=0;i<=a;i++)
+        {
+            for(j=a;j>=i;j--)
+            {
+            System.out.print("* ");
+            }
+            System.out.println();
+            
+        }
+        for(i=2;i<=a;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    
+        }
+   
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        call(a);
+    }
+    
+}
+
+
+
+
+
+output
+5
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
