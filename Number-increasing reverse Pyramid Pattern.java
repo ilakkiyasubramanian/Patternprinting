32333d53e0ad0ba8b7fe61ff9bba7db9dc04a03e
Ilakkiya S <114014932+ilakkiyasubramanian@users.noreply.github.com>
2024-08-01 21:13:38 +0530

    Create Number-increasing reverse Pyramid Pattern.java

---
+++ Number-increasing reverse Pyramid Pattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void print(int a)
+    {
+        int i,j;
+        for(i=a;i>=1;i--)
+        {
+            /*for(j=1;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            */
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }System.out.println();
+        }
+    }
+    
+    
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    print(a);
+		
+	}
+}
